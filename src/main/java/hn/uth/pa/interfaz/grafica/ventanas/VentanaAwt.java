f7589d1076beb4c36297dc1fa16d0ad1679d2342
omaldonado22 <orlynm10@gmail.com>
2023-06-01 16:23:50 -0600

    culo2

--- src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaAwt.java
+++ src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaAwt.java
@@ -1,14 +1,15 @@
 
 package hn.uth.pa.interfaz.grafica.ventanas;
-import java.awt.Frame;
+import javax.swing.JFrame;
 
-public class VentanaAwt extends Frame {
+public class VentanaAwt extends JFrame {
 
     public VentanaAwt() {
         super();
         this.setTitle("Ventana AWT");
         this.setSize(400, 400);
         this.setVisible(true);
+      
     }
     
     
--- src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaSwing.java
+++ src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaSwing.java
@@ -1,26 +1,25 @@
-
 package hn.uth.pa.interfaz.grafica.ventanas;
 import javax.swing.JFrame;
+import javax.swing.JButton;
+ import java.awt.Container;
 
+public class VentanaSwing extends JFrame
+{
+    private JButton b2;
+    public VentanaSwing()
 
-public class VentanaSwing extends JFrame {
-
-    public VentanaSwing() {
+    {
         super();
-        this.setTitle("Ventana Swing");
-        this.setSize(400,400);
+        this.setTitle("Ventana con Swing");
+        this.setSize(300,300);
+        this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        //siiiiii
-        
-     
-        
-  
-        
-        
-        
-    }
+        b2 = new JButton("PRUEBA");
+        Container contenedor = this.getContentPane();
+        contenedor.add(b2);
+  }
+
     
-    
-    
+
 }
