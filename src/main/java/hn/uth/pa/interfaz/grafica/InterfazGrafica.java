00d9ad6c31ee2c2e0b2de998c91e3b8d3a434857
omaldonado22 <orlynm10@gmail.com>
2023-06-01 09:58:36 -0600

    culo2

---
+++ src/main/java/hn/uth/pa/interfaz/grafica/InterfazGrafica.java
@@ -0,0 +1,14 @@
+
+package hn.uth.pa.interfaz.grafica;
+import hn.uth.pa.interfaz.grafica.ventanas.VentanaAwt;
+import hn.uth.pa.interfaz.grafica.ventanas.VentanaSwing;
+
+public class InterfazGrafica {
+
+    public static void main(String[] args) {
+        
+        new VentanaAwt();
+        new VentanaSwing();
+        
+    }
+}
---
+++ src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaSwing.java
@@ -0,0 +1,26 @@
+
+package hn.uth.pa.interfaz.grafica.ventanas;
+import javax.swing.JFrame;
+
+
+public class VentanaSwing extends JFrame {
+
+    public VentanaSwing() {
+        super();
+        this.setTitle("Ventana Swing");
+        this.setSize(400,400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //siiiiii
+        
+     
+        
+  
+        
+        
+        
+    }
+    
+    
+    
+}
